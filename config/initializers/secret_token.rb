5405917d58d08ac3e5628e618767c3e1304964d9
iurevych <sergii.iurevych@railsware.com>
2011-09-05 10:14:22 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailObserver::Application.config.secret_token = '973f090876c2edaa6d92994144b5501ecab2fb9ab1b1a772d551376df828bbc5d9f16a415d259b479ae4d75c4ab52dc05365db567acb228bfa50e47467ddf2b3'
